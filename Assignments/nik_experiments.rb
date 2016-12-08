b29cd5d2ed57f0b9d14335a4a5c49d1b80a3de5a
Nikhil Patil <indieNik@users.noreply.github.com>
2016-12-08 17:58:47 +0530

    Create nik_experiments.rb

---
+++ Assignments/nik_experiments.rb
@@ -0,0 +1,68 @@
+#First:
+
+#cur_weather = %Q{ It's a hot day outside 
+#Grab your umbrellas...}
+#
+#cur_weather.lines do |line|
+#	line.sub! 'hot', 'rainy'
+#	p "#{line}" # Actual Line
+#	p "#{line.strip}" # removes \n and any leading and trailing space
+#	p "#{line.lstrip}" # removes the leading space
+#	p "#{line.rstrip}" # removes the trailing space and \n
+#	p "#{line.chomp}" # removes only \n
+#end
+#
+###Output:
+#
+## => " It's a rainy day outside \n"
+## => "It's a rainy day outside"
+## => "It's a rainy day outside \n"
+## => " It's a rainy day outside"
+## => " It's a rainy day outside "
+## => "Grab your umbrellas..."
+## => "Grab your umbrellas..."
+## => "Grab your umbrellas..."
+## => "Grab your umbrellas..."
+## => "Grab your umbrellas..."
+
+#Second
+#case 19
+#	when 0..18 then p "Minor"
+#	else p "Adult"
+#end
+
+#Third
+#my_hash = Hash.new("No such key yet")
+#p my_hash[0] # "No such key yet"
+#p my_hash["some_key"] # "No such key yet"
+
+#Fourth
+#def adjust (props = {fore: "red" , back: "white"})
+#	puts "Foreground: #{props[:fore]}" if props[:fore]
+#	puts "Background: #{props[:back]}" if props[:back]
+#end
+#
+#adjust
+#adjust ({:fore => "green"})
+#adjust back: "yella"
+#adjust :back => "yella"
+
+class Person
+	attr_accessor :name
+	attr_reader :age
+
+	def initialize (name, ageVar)
+		@name = name
+#		self.age = ageVar
+		@age = age unless age > 100
+		p age
+	end
+#	def age= age
+#		@age = age unless age > 100
+#	end
+end
+
+p1 = Person.new("John", 20)
+p "#{p1.name} #{p1.age}"
+p1.age = 123
+p p1.age
